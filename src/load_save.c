5e2d28047fbeb38d1e81f8464dfae79563af219c
ivaantxo <ivan_martinez_220596@hotmail.com>
2024-02-13 14:50:54 +0100

    Revert "Save-compatible SaveBlock3 (#4112)"
    This reverts commit 311d732359e6ad2cad73c6248a5c7e04fa224f89.

--- src/load_save.c
+++ src/load_save.c
@@ -29,7 +29,6 @@ struct LoadedSaveData
 };
 
 // EWRAM DATA
-EWRAM_DATA struct SaveBlock3 gSaveblock3 = {};
 EWRAM_DATA struct SaveBlock2ASLR gSaveblock2 = {0};
 EWRAM_DATA struct SaveBlock1ASLR gSaveblock1 = {0};
 EWRAM_DATA struct PokemonStorageASLR gPokemonStorage = {0};
@@ -40,7 +39,6 @@ EWRAM_DATA struct LoadedSaveData gLoadedSaveData = {0};
 bool32 gFlashMemoryPresent;
 struct SaveBlock1 *gSaveBlock1Ptr;
 struct SaveBlock2 *gSaveBlock2Ptr;
-IWRAM_INIT struct SaveBlock3 *gSaveBlock3Ptr = &gSaveblock3;
 struct PokemonStorage *gPokemonStoragePtr;
 
 // code
@@ -55,11 +53,6 @@ void CheckForFlashMemory(void)
     {
         gFlashMemoryPresent = FALSE;
     }
-}
-
-void ClearSav3(void)
-{
-    CpuFill16(0, &gSaveblock3, sizeof(struct SaveBlock3));
 }
 
 void ClearSav2(void)
